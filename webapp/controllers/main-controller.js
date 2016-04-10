f53619802046ba38a2fc0e71c7f63a4f9e34f23d
Jasna Blemberg <jasnab@rainn.org>
2016-04-09 21:49:46 -0400

    Add edit bookmark formwith angular(copy)

--- webapp/controllers/main-controller.js
+++ webapp/controllers/main-controller.js
@@ -17,6 +17,10 @@ function MainCtrl() {
     mainVm.cancelEditing = cancelEditing;
     mainVm.createBookmark = createBookmark;
     mainVm.resetCreateForm = resetCreateForm;
+    mainVm.editedBookmark = null;
+    mainVm.setEditedBookmark = setEditedBookmark;
+    mainVm.updateBookmark = updateBookmark;
+    mainVm.isSelectedBookmark = isSelectedBookmark;
 
     mainVm.categories = [
         {"id": 0, "name": "Development"},
@@ -90,7 +94,23 @@ function MainCtrl() {
             url: '',
             category: mainVm.currentCategory.name
         }
+    }
 
+    function setEditedBookmark(bookmark) {
+        mainVm.editedBookmark = angular.copy(bookmark);
+    }
+
+    function updateBookmark(bookmark) {
+        var index =_.findIndex(mainVm.bookmarks, function(b) {
+            return b.id === bookmark.id;
+        });
+        mainVm.bookmarks[index] = bookmark;
+        mainVm.editedBookmark = null;
+        mainVm.isEditing = false;
+    }
+
+    function isSelectedBookmark(bookmarkId) {
+        return mainVm.editedBookmark !== null && mainVm.editedBookmark.id === bookmarkId;
     }
 
 }
